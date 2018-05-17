e3288201f4ab0bb1962daf916b61b8df3ff1885c
Erick A. Montañez <codeams@gmail.com>
2018-05-17 01:59:19 -0500

    Clean the utils file

--- utils.py
+++ utils.py
@@ -1,9 +1,6 @@
 
-# Removes system hidden file
-# names from the given array.
-# Supports macOS X.10+
-def remove_system_files(list):
-    for element in list:
-        if element == ".DS_Store":
-            list.remove(element)
-    return list
+def remove_system_files(file_paths):
+    for file_path in file_paths:
+        if file_path == ".DS_Store":
+            file_paths.remove(file_path)
+    return file_paths
