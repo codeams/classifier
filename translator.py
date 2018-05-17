edf6968905e4383882e6ef64e54d6c885a31428a
Erick A. Montañez <codeams@gmail.com>
2018-05-16 19:45:19 -0500

    Add details to interface

---
+++ translator.py
@@ -0,0 +1,14 @@
+
+def translate_label(label):
+    if label == 'coconut':
+        return 'coco'
+    elif label == 'banana':
+        return 'platano'
+    elif label == 'apple':
+        return 'manzana roja'
+    elif label == 'blueberry':
+        return 'mora azul'
+    elif label == 'avocado':
+        return 'aguacate'
+    else:
+        return 'Desconocido'
