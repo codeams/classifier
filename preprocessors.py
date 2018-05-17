1be0daf6d289ad8c96d4a4d834da639bcfff539f
Erick A. Montañez <codeams@gmail.com>
2018-05-17 02:00:22 -0500

    Fix preprocessors file spacing

--- preprocessors.py
+++ preprocessors.py
@@ -1,12 +1,16 @@
 
 import cv2
+from matplotlib import pyplot
+
 
 def grayscale(image):
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return grayscale_image
 
+
 def gaussian_blur(image):
     return cv2.GaussianBlur(image,(5,5),0)
+
 
 def image_to_feature_vector(image, size=(32, 32)):
     return cv2.resize(image, size).flatten()
